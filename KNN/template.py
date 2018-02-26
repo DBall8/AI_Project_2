64b2a59f48ef3d98ba8b75f7a258a44e7951e42b
Nik Castelino <nmcastelino@wpi.edu>
2018-02-26 18:32:14 -0500

    Slight Changes to KNN

--- KNN/template.py
+++ KNN/template.py
@@ -3,8 +3,8 @@ import numpy.random
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-import matplotlib.pyplot as plt
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
@@ -14,8 +14,8 @@ from sklearn import metrics
 from sklearn.neighbors import NearestNeighbors
 
 
-def printIMG(img):
-    image = img.reshape([28,28])
+def printIMG(image):
+    image = image.reshape([28,28])
     plt.gray()
     plt.imshow(image)
     plt.show()
@@ -23,31 +23,26 @@ def printIMG(img):
 def processIMG(imagePath, labelsPath):
     images = np.load(imagePath)
     image_vectors = []
-
     image_vectors = images.reshape(6500, 784)
     image_vectors = np.array(image_vectors)
 
     labels = np.load(labelsPath)
-    labels_flat = labels.reshape(6500, 1)
+    label = labels.reshape(6500, 1)
     label_vectors = []
-    one_hot_labels = keras.utils.to_categorical(labels_flat, num_classes=10)
-    label_vectors = np.array(one_hot_labels)
-
+    classify_label = keras.utils.to_categorical(label, num_classes=10)
+    label_vectors = np.array(classify_label)
     data = np.column_stack((label_vectors, image_vectors))
     np.random.shuffle(data)
+    classify_label = data[:,0:10]
+    end_images = data[:,10:]
 
-    one_hot_labels = data[:,0:10]
-    flattend_images = data[:,10:]
-
-    data_size = 6500
-    training_size = int(data_size * 0.60)
-    print ("Training Set size = ", training_size)
-    val_size = int(data_size * (0.65 + 0.15))
-    print ("Validation set index = ", val_size)
+    data_length = 7000
+    train = int(data_length * 0.60)
+    value = int(data_length * (0.65 + 0.15))
 
     num_classes = np.unique(labels).shape[0]
-    x_train, x_val, x_test = flattend_images[:training_size], flattend_images[training_size:val_size], flattend_images[val_size:]
-    y_train, y_val, y_test = one_hot_labels[:training_size], one_hot_labels[training_size: val_size], one_hot_labels[val_size:]
+    x_train, x_val, x_test = end_images[:train], end_images[train:value], end_images[value:]
+    y_train, y_val, y_test = classify_label[:train], classify_label[train: value], classify_label[value:]
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 
@@ -56,20 +51,18 @@ def processIMG(imagePath, labelsPath):
 x_train, y_train, x_val, y_val, x_test, y_test = processIMG("images.npy","labels.npy")
 
 knn = KNeighborsClassifier(n_neighbors=3)
-
 knn.fit(x_train, y_train)
-
 expected = y_test
-
 predicted = knn.predict(x_test)
 
 a = knn.kneighbors(n_neighbors=3, return_distance=False)
-printIMG(x_train[a[0][0]])
-printIMG(x_train[a[0][1]])
-printIMG(x_train[a[0][2]])
+#printIMG(x_train[a[0][0]])
+#printIMG(x_train[a[0][1]])
+#printIMG(x_train[a[0][2]])
+#printIMG(x_train[a[0][3]])
 
-print("n=3")
-print("Classification")
+print("k=3")
+print("Classification Report")
 print(metrics.classification_report(expected, predicted))
 print ("Confusion Matrix")
 y_act = list(map(np.argmax, expected))
